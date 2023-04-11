27f70edfdc1511da63b0d788d6d372310b0126f0
Serhii Revva <besha2vox@gmail.com>
2023-04-11 11:28:57 +0300

    qwe

--- src/services/validateUniq.js
+++ src/services/validateUniq.js
@@ -11,14 +11,19 @@ const { Contact } = require('../schemas');
 // };
 
 const validateUniq = async (next, contact) => {
-    console.log('Contact in validateUniq: ', Contact);
-    const existingContact = await Contact.findOne({ name: contact.name });
-    if (existingContact) {
-        const err = new Error('Name must be unique');
-        err.status = 400;
-        return next(err);
+    try {
+        const existingContact = await Contact.findOne({ name: contact.name });
+        if (existingContact) {
+            const err = new Error('Name must be unique');
+            err.status = 400;
+            return next(err);
+        }
+        next();
+    } catch (error) {
+        console.log('Contact in validateUniq: ', Contact);
+        error.status = 500;
+        next(error);
     }
-    next();
 };
 
 module.exports = validateUniq;
